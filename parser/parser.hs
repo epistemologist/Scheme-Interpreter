3f9f1a83371a81610d1e7d7fbe422e0db6af6dad
Husnain Raza <hhrraazzaa@gmail.com>
2020-08-05 02:20:46 -0500

    Parsing/Writing a Simple Parser Exercise 1 complete

--- parser/parser.hs
+++ parser/parser.hs
@@ -1,7 +1,7 @@
 module Main where
 import Text.ParserCombinators.Parsec hiding (spaces) -- import Parsec without the spaces function
 import System.Environment
-
+import Control.Monad
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~" -- parser that matches one of the characters in the given string, and gives error otherwise
 
@@ -9,11 +9,9 @@ spaces :: Parser ()
 spaces = skipMany1 space -- parser that recognizes 1 or more spaces
 
 readExpr :: String -> String -- readExpr is a function that takes in a string and returns a string
-readExpr input = case parse (spaces >> symbol) "lisp" input of -- input-parameter, "lisp" is the name of our input
--- note, we can write (spaces >> symbol) to pass input from spaces to symbol
--- generally, a >> b is equivalent to do a b
+readExpr input = case parse parseExpr "lisp" input of -- input-parameter, "lisp" is the name of our input
 	Left err -> "No match: " ++ show err
-	Right val -> "Found value: " ++ show val
+	Right val -> "Found value"
 
 -- Defining a datatype for any kind of Lisp value (similar to an enum)
 data LispVal = Atom String -- Atom: stores String naming the atom
@@ -28,14 +26,43 @@ parseString = do -- do the following statements
 				char '"' -- look for a single double quote
 				x <- many (noneOf "\"") -- assign to x as many non-double quote characters as possible
 				char '"' -- look for another single quote
-				return $ String x -- return x
+				return (String x) -- return x casted as a String
 
+parseAtom :: Parser LispVal -- parser for Atom
+parseAtom = do -- do the following actions
+			  first <- letter <|> symbol -- try to run letter and assign it to first, if that fails, then run symbol and assign it to first
+			  rest <- many (letter <|> digit <|> symbol) -- recieve as many letters, digits, or symbols as there are
+			  let atom = first:rest -- colon is list concatenation
+			  return $ case atom of
+			  			"#t" -> Bool True -- special case, "#t" is True in Scheme
+			  			"#f" -> Bool False -- and "#f" is False in scheme
+			  			_ -> Atom atom -- default case
+{-
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+-- "many1 digit" - matches 1 or more digits
+--  "(Number . read) $ many1 digit" == "Number read many1 digit" - 
+-- note that many1 digit returns a Parser String so we need liftM to operate on (Number.read) for things to work
+-}
+
+-- Exercise 1: rewriting parseNumber
+parseNumber :: Parser LispVal
+parseNumber = do
+			    digits <- many1 digit
+			    let n = read digits :: Integer
+			    return (Number n)
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+{-
 test :: String -> String
-test input = case parse (parseString) "lisp" input of
+test input = case parse (parseNumber) "lisp" input of
 			 Left err -> "No match: " ++ show err
-			 Right val -> "Found value: " ++ val
+			 Right val -> "Found value: " ++ show val
+-}
 
 main :: IO ()
 main = do
 	(expr:_) <- getArgs -- get the args and assign the first element to expr
-	putStrLn (test expr)
+	putStrLn (readExpr expr)
