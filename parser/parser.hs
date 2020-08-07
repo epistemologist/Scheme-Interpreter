2e93e8cd968ed353b038bb65b41d71bc922beec2
Husnain Raza <hhrraazzaa@gmail.com>
2020-08-07 17:46:11 -0500

    Added parsing for numbers

--- parser/parser.hs
+++ parser/parser.hs
@@ -1,12 +1,13 @@
 module Main where
 
 import Control.Monad
+import Numeric
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 -- valid Scheme symbols
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -25,6 +26,7 @@ data LispVal
   | Number Integer
   | String String
   | Bool Bool
+  deriving (Show)
 
 -- escaped chars helper parser for parseString
 escapedChars :: Parser Char
@@ -32,11 +34,11 @@ escapedChars = do
   char '\\'
   x <- oneOf "\\\"nrt"
   return $ case x of
-  	'\\' -> x
-  	'\"' -> x
-  	'n' -> '\n'
-  	'r' -> '\r'
-  	't' -> '\t'
+    '\\' -> x
+    '\"' -> x
+    'n' -> '\n'
+    'r' -> '\r'
+    't' -> '\t'
 
 -- parser for String
 parseString :: Parser LispVal
@@ -52,25 +54,68 @@ parseAtom = do
   first <- letter <|> symbol
   rest <- many (letter <|> digit <|> symbol)
   let atom = first : rest
-  return $ case atom of
-    "#t" -> Bool True
-    "#f" -> Bool False
-    _ -> Atom atom
+  return (Atom atom)
+
+-- parser for Bool
+parseBool :: Parser LispVal
+parseBool = do
+  char '#'
+  (char 't' >> return (Bool True)) <|> (char 'f' >> return (Bool False))
 
 -- parser for Number
 parseNumber :: Parser LispVal
-parseNumber = do
+parseNumber = parseNumberNormal <|> parseDec <|> parseOct  <|> parseHex <|> parseBin
+
+parseNumberNormal :: Parser LispVal
+parseNumberNormal = do
   digits <- many1 digit
   let n = read digits :: Integer
-  return (Number n)
+  return $ Number n
+
+parseDec :: Parser LispVal
+parseDec = do
+  try (string "#d")
+  digits <- many1 digit
+  let n = read digits :: Integer
+  return $ Number n
+
+parseOct :: Parser LispVal
+parseOct = do
+  try (string "#o")
+  s <- many1 octDigit
+  let n = fst $ readOct s !! 0
+  return $ Number n
+
+parseHex :: Parser LispVal
+parseHex = do
+  try (string "#x")
+  s <- many1 hexDigit
+  let n = fst $ readHex s !! 0
+  return $ Number n
+
+parseBin :: Parser LispVal
+parseBin = do
+  try (string "#b")
+  s <- many1 (oneOf "01")
+  let n = binToDec s
+  return $ Number n
+
+binToDec :: String -> Integer
+binToDec s = binToDec' $ reverse s
+  where
+    binToDec' "0" = 0
+    binToDec' "1" = 1
+    binToDec' (x : xs)
+      | x == '0' = 2 * binToDec' xs
+      | x == '1' = 1 + 2 * binToDec' xs
 
 -- parser for expression
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
+parseExpr = parseAtom <|> parseString <|> parseNumber <|> parseBool
 
 -- test function to test parser
 test :: String -> String
-test input = case parse (escapedChars) "lisp" input of
+test input = case parse (parseExpr) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value: " ++ show val
 
@@ -79,3 +124,4 @@ main :: IO ()
 main = do
   (expr : _) <- getArgs
   putStrLn (readExpr expr)
+
